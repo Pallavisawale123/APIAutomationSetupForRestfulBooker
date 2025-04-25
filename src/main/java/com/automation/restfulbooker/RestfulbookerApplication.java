07c8add429d9a5b3f578f9ea6d79dbf8c28ee4c5
sawale pallavi <53057367+Pallavisawale123@users.noreply.github.com>
2025-04-25 14:22:42 -0700

    Initial commit

---
+++ src/main/java/com/automation/restfulbooker/RestfulbookerApplication.java
@@ -0,0 +1,13 @@
+package com.automation.restfulbooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulbookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulbookerApplication.class, args);
+	}
+
+}
